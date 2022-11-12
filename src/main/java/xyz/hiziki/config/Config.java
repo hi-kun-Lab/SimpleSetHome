d136947336ca06f445ab6da1fa7a91ba3f84bde1
hi-kun-JPN <bbremon321@gmail.com>
2022-11-13 07:32:26 +0900

    Configの変更に準じて変更

--- src/main/java/xyz/hiziki/config/Config.java
+++ src/main/java/xyz/hiziki/config/Config.java
@@ -29,6 +29,8 @@ public class Config
         if (!config.contains("enable-set-home-sound") || !config.contains("enable-teleport-sound") ||
                 !config.contains("enable-set-home-message") || !config.contains("enable-teleport-message") ||
                 !config.contains("set-home-message") || !config.contains("teleport-message") ||
+                !config.contains("enable-set-home-delay") || !config.contains("enable-teleport-delay") ||
+                !config.contains("set-home-delay(s)") || !config.contains("teleport-delay(s)") ||
                 !config.contains("max-home"))
         {
             plugin.getLogger().info("config.yml にエラーが起こっています。");
@@ -49,6 +51,16 @@ public class Config
             plugin.getLogger().info("set-home-messageがString形じゃありません。");
         }
 
+        if (!config.isBoolean("enable-set-home-delay"))
+        {
+            plugin.getLogger().info("enable-set-home-delayがBoolean形じゃありません。");
+        }
+
+        if (!config.isInt("set-home-delay(s)"))
+        {
+            plugin.getLogger().info("set-home-delay(s)がInt形じゃありません。");
+        }
+
         if (!config.isBoolean("enable-teleport-sound"))
         {
             plugin.getLogger().info("enable-teleport-soundがBoolean形じゃありません。");
@@ -64,6 +76,16 @@ public class Config
             plugin.getLogger().info("teleport-messageがString形じゃありません。");
         }
 
+        if (!config.isBoolean("enable-teleport-delay"))
+        {
+            plugin.getLogger().info("enable-teleport-delayがBoolean形じゃありません。");
+        }
+
+        if (!config.isInt("teleport-delay(s)"))
+        {
+            plugin.getLogger().info("teleport-delay(s)がInt形じゃありません。");
+        }
+
         if (!config.isInt("max-home"))
         {
             plugin.getLogger().info("max-homeがInt形じゃありません。");
@@ -76,11 +98,15 @@ public class Config
 //enable-set-home-sound: true
 //enable-set-home-message: true
 //set-home-message: 'ホームを設定しました。'
+//enable-set-home-delay: false
+//set-home-delay(s): 5
 //
 //# --- ホームにTPした時 ---#
 //enable-teleport-sound: true
 //enable-teleport-message: true
 //teleport-message: 'ホームにTPしました。'
+//enable-teleport-delay: true
+//teleport-delay(s): 5
 //
 //# --- 最大ホーム数 --- #
 //max-home: 3
--- src/main/java/xyz/hiziki/config/ConfigFile.java
+++ src/main/java/xyz/hiziki/config/ConfigFile.java
@@ -10,40 +10,27 @@ public class ConfigFile
 
     private final FileConfiguration config = plugin.getConfig();
 
-    public boolean getENABLE_SET_HOME_SOUND()
-    {
-        return config.getBoolean("enable-set-home-sound");
-    }
+    public boolean getENABLE_SET_HOME_SOUND() { return config.getBoolean("enable-set-home-sound"); }
 
-    public boolean getENABLE_SET_HOME_MESSAGE()
-    {
-        return config.getBoolean("enable-set-home-message");
-    }
+    public boolean getENABLE_SET_HOME_MESSAGE() { return config.getBoolean("enable-set-home-message"); }
 
-    public String getSET_HOME_MESSAGE()
-    {
-        return config.getString("set-home-message");
-    }
+    public String getSET_HOME_MESSAGE() { return config.getString("set-home-message"); }
 
-    public boolean getENABLE_TELEPORT_SOUND()
-    {
-        return config.getBoolean("enable-teleport-sound");
-    }
+    public boolean getENABLE_SET_HOME_DELAY() { return config.getBoolean("enable-set-home-delay"); }
 
-    public boolean getENABLE_TELEPORT_MESSAGE()
-    {
-        return config.getBoolean("enable-teleport-message");
-    }
+    public int getSET_HOME_DELAY() { return config.getInt("set-home-delay(s)"); }
 
-    public String getTELEPORT_MESSAGE()
-    {
-        return config.getString("teleport-message");
-    }
+    public boolean getENABLE_TELEPORT_SOUND() { return config.getBoolean("enable-teleport-sound"); }
 
-    public int getMAX_HOME()
-    {
-        return config.getInt("max-home");
-    }
+    public boolean getENABLE_TELEPORT_MESSAGE() { return config.getBoolean("enable-teleport-message"); }
+
+    public String getTELEPORT_MESSAGE() { return config.getString("teleport-message"); }
+
+    public boolean getENABLE_TELEPORT_DELAY() { return config.getBoolean("enable-teleport-delay"); }
+
+    public int getTELEPORT_DELAY() { return config.getInt("teleport-delay(s)"); }
+
+    public int getMAX_HOME() { return config.getInt("max-home"); }
 
     //# --- 設定ファイル --- #
     //
@@ -51,11 +38,15 @@ public class ConfigFile
     //enable-set-home-sound: true
     //enable-set-home-message: true
     //set-home-message: 'ホームを設定しました。'
+    //enable-set-home-delay: false
+    //set-home-delay(s): 5
     //
     //# --- ホームにTPした時 ---#
     //enable-teleport-sound: true
     //enable-teleport-message: true
     //teleport-message: 'ホームにTPしました。'
+    //enable-teleport-delay: true
+    //teleport-delay(s): 5
     //
     //# --- 最大ホーム数 --- #
     //max-home: 3
