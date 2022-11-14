872d47fb987c1f19f8da17be5ed93e0125630d4a
hi-kun-JPN <bbremon321@gmail.com>
2022-11-15 05:19:25 +0900

    Disable時にファイルを保存する必要はないので削除

--- src/main/java/xyz/hiziki/Main.kt
+++ src/main/java/xyz/hiziki/Main.kt
@@ -4,7 +4,6 @@ import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.plugin.java.JavaPlugin
 import xyz.hiziki.command.CommandManager
 import xyz.hiziki.config.Config
-import xyz.hiziki.util.SaveFile
 import java.io.File
 
 class Main : JavaPlugin()
@@ -13,19 +12,23 @@ class Main : JavaPlugin()
     {
         //プラグインが起動する時
         super.onEnable()
+
         plugin = this
+
         homesFile = File(dataFolder, "Homes.yml")
         homes = YamlConfiguration.loadConfiguration(homesFile!!)
+
         Config(this)
         CommandManager(this)
+
         logger.info("プラグインは正常に起動しました。")
     }
 
     override fun onDisable()
     {
         //プラグインが停止する時
-        super.onDisable() //俺もよくわからんけど書いてる(書かないといけない)らしい
-        SaveFile() //ファイルを保存する
+        super.onDisable()
+
         logger.info("プラグインは正常に停止しました。")
     }
 
